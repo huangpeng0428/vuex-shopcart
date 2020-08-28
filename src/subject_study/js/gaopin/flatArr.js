709c24722e008869a3c721a1ca93cc7ce9b1d2f0
huangpeng <huangpeng@meizu.com>
2020-08-28 17:43:33 +0800

    faltArr #0

---
+++ src/subject_study/js/gaopin/flatArr.js
@@ -0,0 +1,37 @@
+/*
+ * @Date: 2020-08-28 17:11:48
+ * @LastEditors: PoloHuang
+ * @LastEditTime: 2020-08-28 17:42:12
+ */
+const arr = [1, [[2], [3, [4]], 5]]
+
+/**
+ * @description: 递归
+ * @param {type}
+ * @return {type}
+ * @author: PoloHuang
+ */
+const flatten1 = function(arr) {
+    return Array.isArray(arr) ? arr.reduce((prev, item) => {
+        return [...prev, ...flatten1(item)]
+    }, []) : [arr]
+}
+
+// const a = flatten1(arr)
+// console.log(a)
+
+/**
+ * @description: 迭代
+ * @param {type}
+ * @return {type}
+ * @author: PoloHuang
+ */
+
+ const flatten2 = function(arr) {
+    while (arr.some(item => Array.isArray(item))) {
+        arr = [].concat(...arr)
+    }
+    return arr
+ }
+ const b = flatten2(arr)
+ console.log(b)
